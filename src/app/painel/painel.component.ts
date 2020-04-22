669496ad9826b3b38c651cb586df60d6afdea83f
pedro <zpedroluis@outlook.com>
2020-04-22 16:23:12 -0300

    Adicionado o print do que o usuario digita na textarea utilizando a tecnica de Event Binding

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,14 +12,15 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   instrucao: string = 'Traduza a frase'
+  public resposta: string
 
   constructor() { console.log(this.frases) }
 
   ngOnInit() {
   }
 
-
-  public atualizaResposta(): void {
-    console.log('teste')
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)
   }
 }
