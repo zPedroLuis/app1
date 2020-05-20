3a4d562d94453e5d434feca0d16f1e42a2c751ca
pedro <zpedroluis@outlook.com>
2020-05-20 16:57:12 -0300

    feat: Property Binding das tentativas.

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Coracao } from '../shared/coracao.model'
 
@@ -13,14 +13,17 @@ export class TentativasComponent implements OnInit {
   public coracaoVazio: string = '/assets/coracao_vazio.png'
   public coracaoCheio: string = '/assets/coracao_cheio.png'
 
+  @Input() public tentativas: number
   public coracoes: Coracao[] = [
     new Coracao(true), new Coracao(true), new Coracao(true)
   ]
   constructor() {
     console.log(this.coracoes)
+
   }
 
   ngOnInit() {
+    console.log('Tentativas recebidas do painel: ', this.tentativas)
   }
 
 }
