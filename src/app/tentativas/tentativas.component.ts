9ba1f54666067b57f929c90b45323f9f34777ed4
pedro <zpedroluis@outlook.com>
2020-05-26 16:29:39 -0300

    feat: Adicionado o ngOnChanges() e removidos os console.logs

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 import { Coracao } from '../shared/coracao.model'
 
@@ -8,7 +8,7 @@ import { Coracao } from '../shared/coracao.model'
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges{
 
   public coracaoVazio: string = '/assets/coracao_vazio.png'
   public coracaoCheio: string = '/assets/coracao_cheio.png'
@@ -22,8 +22,12 @@ export class TentativasComponent implements OnInit {
 
   }
 
-  ngOnInit() {
+  ngOnChanges() {
     console.log('Tentativas recebidas do painel: ', this.tentativas)
+  }
+
+  ngOnInit() {
+
   }
 
 }
